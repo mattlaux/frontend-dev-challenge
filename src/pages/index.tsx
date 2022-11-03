0521e728c91b67cec14a7ba7c99db0dbe1ace269
drattylight <mlaux6@outlook.com>
2022-11-03 10:30:14 -0500

    Create and style school card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+
+type SchoolCardProps = {
+  universityName: string;
+  location: string;
+};
+
+function SchoolCard(props: SchoolCardProps): JSX.Element {
+  return (
+    <section className={styles.schoolCard}>
+      <div className={styles.initialContainer}>
+        <p className={styles.initial}>
+          {props.universityName.charAt(0).toUpperCase()}
+        </p>
+      </div>
+      <h2 className={styles.h2Text}>{props.universityName}</h2>
+      <p className={styles.locationText}>{props.location}</p>
+    </section>
+  );
+}
 
 export default function Home() {
   return (
-    <div></div>
-  )
+    <main className={styles.container}>
+      <h1>Pick Your School</h1>
+      <SchoolCard
+        universityName="Test School"
+        location="Test Location"
+      ></SchoolCard>
+    </main>
+  );
 }
