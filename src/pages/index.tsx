d810f5bcb2504752e6b6cc36034a74c7f4d907fa
drattylight <mlaux6@outlook.com>
2022-11-03 11:11:20 -0500

    Code and style search bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,52 @@
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import Image from "next/image";
 
 type SchoolCardProps = {
   universityName: string;
   location: string;
 };
 
+type SearchBarProps = {
+  searchBarValue: string;
+  handleSearchBarValue: (searchBarInput: string) => void;
+};
+
+/**
+ * React controlled input (State handled by parent React Container instead of HTML DOM. Best practice)
+ * https://reactjs.org/docs/forms.html#controlled-components
+ * @param universityName - University name from API
+ * @param location - Location from API
+ * @returns Search bar for user to search through universities returned from API
+ */
+function SearchBar(props: SearchBarProps): JSX.Element {
+  return (
+    <div className={styles.searchBarContainer}>
+      <Image
+        src="/search.svg"
+        alt="Magnifying glass for search bar"
+        width={18}
+        height={19}
+      />
+      <input
+        type="text"
+        placeholder="Search for your school..."
+        value={props.searchBarValue}
+        onChange={(e) => {
+          props.handleSearchBarValue(e.target.value);
+        }}
+        className={styles.searchBar}
+      ></input>
+    </div>
+  );
+}
+
+/**
+ *
+ * @param searchBarValue - Current value of search bar. This is held in React state in Home component
+ * @param handleSearchBarValue - Updates search bar state in Home component
+ * @returns School card component with info received from API
+ */
 function SchoolCard(props: SchoolCardProps): JSX.Element {
   return (
     <section className={styles.schoolCard}>
@@ -20,9 +62,23 @@ function SchoolCard(props: SchoolCardProps): JSX.Element {
 }
 
 export default function Home() {
+  const [searchBarValue, setSearchBarValue] = useState("");
+
+  /**
+   * Event handler that is passed to SearchBar component
+   * @param searchBarInput - User input from search bar
+   */
+  function handleSearchBar(searchBarInput: string) {
+    setSearchBarValue(searchBarInput);
+  }
+
   return (
     <main className={styles.container}>
       <h1>Pick Your School</h1>
+      <SearchBar
+        searchBarValue={searchBarValue}
+        handleSearchBarValue={handleSearchBar}
+      />
       <SchoolCard
         universityName="Test School"
         location="Test Location"
